d13e0a6613eca60ed2b901ef59318e411ea801eb
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-22 13:31:49 +0100

    maj

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 def np_shape(matrix):
-    shape = [len(matrix)]
-    test = matrix[0]
-    while (isinstance(test, list)):
-        shape.append(len(test))
-        test = test[0]
-    return (tuple(shape))
+    return matrix.shape
