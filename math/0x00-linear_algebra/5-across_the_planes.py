a876834a6abb14564ea4a02a86e36bf73ff17de3
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-22 11:00:51 +0100

    maj

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -11,7 +11,7 @@ def add_matrices2D(mat1, mat2):
     if (matrix_shape(mat1) != matrix_shape(mat2)):
         return None
     mat = []
-    for i in range(len(mat1):
+    for i in range(len(mat1)):
         mat.append([])
         for j in range(len(mat1[0])):
         mat[i].append(mat1[i][j] + mat2[i][j])
