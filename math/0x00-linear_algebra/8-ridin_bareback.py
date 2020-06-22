20903fc141bb5caf2a9b5e9cd051b6da5b32dcd3
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-22 12:52:21 +0100

    maj

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -2,12 +2,12 @@
 def mat_mul(mat1, mat2):
     if (len(mat1[0]) != len(mat2)):
         return None
-    mat=[]
+    mat=[0]*len(mat1)
     for i in range(len(mat1)):
-        mat.append([])
+        mat.append([0]*len(mat2[0]))
 	for j in range(len(mat2[0])):
             s = 0
             for k in range(len(mat1[0])):
                 s = s + (mat1[i][k] * mat2[k][j])
-            mat[i].append(s)
+            mat[i][j] = s
     return mat
