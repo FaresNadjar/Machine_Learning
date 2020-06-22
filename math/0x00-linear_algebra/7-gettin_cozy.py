4c882e88004ac21b754d7406d75d69a6adf26849
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-22 11:44:30 +0100

    maj

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 def cat_matrices2D(mat1, mat2, axis=0):
+    if (axis == 0 and (len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0])):
+        return None
     mat = []
     for i in mat1 :
         mat.append(i.copy())
