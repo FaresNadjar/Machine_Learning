11cb283bde13c2d0ee73822823c4cb9707b4c24e
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-22 09:41:38 +0100

    maj

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
-"""Retrieve a Matrix's Shape
-Returns:
-list: Matrix's Shape
-"""
-
 def matrix_shape(matrix):
-
     shape = [len(matrix)]
     test = matrix[0]
-
     while (isinstance(test, list)):
         shape.append(len(test))
         test = test[0]
