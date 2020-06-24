734cca4b912f0b67d87e413fb229c316b644c9c2
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-24 10:30:24 +0100

    maj

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import numpy as np
 def np_cat(mat1, mat2, axis=0):
-    new = np.array(np.copy(mat1))
-    np.concatenate(new, np.copy(mat2), axis)
+    new = np.concatenate((mat1, mat2), axis)
     return new
