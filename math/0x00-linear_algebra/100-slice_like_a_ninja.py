7cd744f74bb16b907d31a72a87140bde7f439395
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-25 13:09:06 +0100

    maj

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -4,21 +4,21 @@ def np_slice(matrix, axes={}):
     for i in axes.keys() :
         if i == 0 :
             if len(axes[i]) == 1 :
-                new = new[axes[i][0]:]
+                new = new[:axes[i][0]]
             elif len(axes[i]) == 2 :
                 new = new[axes[i][0]:axes[i][1]]
             else :
                 new = new[::axes[i][2]]
         elif i == 1 :
             if len(axes[i]) == 1 :
-                new = new[:,axes[i][0]:]
+                new = new[:,:axes[i][0]]
             elif len(axes[i]) == 2 :
                 new = new[:,axes[i][0]:axes[i][1]]
             else :
                 new = new[:,::axes[i][2]]
         elif i == 2 :
             if len(axes[i]) == 1 :
-                new = new[:,:,axes[i][0]:]
+                new = new[:,:,:axes[i][0]]
             elif len(axes[i]) == 2 :
                 new = new[:,:,axes[i][0]:axes[i][1]]
             else :
