cde7b9d8d20e69f173f37980f5c69798a365b645
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-29 16:14:11 +0100

    maj

--- math/0x03-probability/README.md
+++ math/0x03-probability/README.md
@@ -1,2 +1,19 @@
 # Probability
 ## Initialize Poisson
+```python
+#!/usr/bin/env python3
+class Poisson:
+    def __init__(self, data=None, lambtha=1.):
+        if data == None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = lambtha
+        else:
+            if not(isinstance(data, list)):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = sum(data) / len(data)
+```
--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-class poisson:
+class Poisson:
     def __init__(self, data=None, lambtha=1.):
         if data == None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             else:
-                lambtha = self.lambtha
+                self.lambtha = lambtha
         else:
             if not(isinstance(data, list)):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
-                lambtha = sum(data) / len(data)
+                self.lambtha = sum(data) / len(data)
