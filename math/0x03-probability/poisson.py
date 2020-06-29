280bc0540249406f1fe6088c8bec8ec371073055
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-29 16:27:51 +0100

    maj

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 class Poisson:
     def __init__(self, data=None, lambtha=1.):
         if data == None:
@@ -13,3 +14,13 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
             else:
                 self.lambtha = sum(data) / len(data)
+    def pmf(self, k):
+        if not(isinstance(k, int)) :
+            k = int(k)
+        if k <= 0:
+            return 0
+        s = 1
+        for i in range(1, k + 1) :
+            s = s * i
+        res = (self.lambtha**k) * math.exp(-self.lambtha) / s
+        return res
