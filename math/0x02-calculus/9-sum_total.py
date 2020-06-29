e7a829cc44673b34c1709370c3ca3fbee0374283
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-29 14:38:18 +0100

    maj

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,7 +2,6 @@
 def summation_i_squared(n):
     if n <= 0:
         return None
-    s = 0
-    for i in range(1, n + 1):
-        s = s + (i * i)
-    return s
+    if n == 1 :
+        return 1
+    return (n * n * summation_i_squared(n - 1)
