aa3e3a3e191a469bfe71002bd7cf5c495ad36697
NadjarFares <fares.nadjar@holbertonschool.com>
2020-06-29 15:51:31 +0100

    maj

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -3,7 +3,7 @@ def poly_integral(poly, C=0):
     if (not(poly) or not(isinstance(poly, list)) or not(isinstance(C, int))):
         return None
     new = [C]
-    for i in range(1, len(poly)):
+    for i in range(len(poly)):
         if (poly[i] == 0):
             new.append(0)
         else:
