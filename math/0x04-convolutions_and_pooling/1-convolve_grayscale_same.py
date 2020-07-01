fa6768375249f1343ed9da2cf07977e733f04763
NadjarFares <fares.nadjar@holbertonschool.com>
2020-07-01 11:28:01 +0100

    maj

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
+"""
+Same
+"""
+
 import numpy as np
+
+
 def convolve_grayscale_same(images, kernel):
-    l = []
-    for im in images :
-        im = np.pad(im, (1,1), "constant", constant_values=(0,0))
-        res = np.zeros((im.shape[0] - kernel.shape[0] + 1, im.shape[1] - kernel.shape[1] + 1))
-        for i in range(im.shape[0] - kernel.shape[0] + 1):
-            for j in range(im.shape[1] - kernel.shape[1] + 1):
-                res[i,j] = np.sum(im[i:i+kernel.shape[0],j:j+kernel.shape[1]] * kernel)
-        l.append(res)
-    return np.array(l)
+    """Performs a same convolution on grayscale images.
+    Args:
+        images (np.ndarray): matrix of shape (m, h, w) containing multiple
+                             grayscale images.
+        kernel (np.ndarray): matrix of shape (kh, kw) containing the kernel
+                             for the convolution.
+    Returns:
+        np.ndarray: The convolved images.
+    """
+    images = np.pad(images, ((0,0), (1,1), (1,1)), 'constant', constant_values=(0,0))
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    ch = h - kh + 1
+    cw = w - kw + 1
+    
+    res = np.zeros((m, ch, cw))
+    
+    for j in range(ch):
+        for k in range(cw):
+            images_slide = images[:, j:j + kh, k:k + kw]
+            elem_mul = np.multiply(images_slide, kernel)
+            res[:, j, k] = elem_mul.sum(axis=1).sum(axis=1)
+    print(res.shape)
+    return res
