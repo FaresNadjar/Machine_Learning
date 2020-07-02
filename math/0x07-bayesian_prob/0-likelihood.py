1df0056659f84d9826ca7ac6a240af2d60eb50a9
NadjarFares <fares.nadjar@holbertonschool.com>
2020-07-02 15:43:06 +0100

    maj

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 """
-likelihood function
+Module used to
 """
 
 import numpy as np
 
 def likelihood(x, n, P):
     """
-    likelihood function
+    Function that calculates the likelihood of obtaining this data given
+    various hypothetical probabilities of developing severe side effects:
     Args:
-    - x: number of patients that develop severe side effects
-    - n: total number of patients
-    - P: 1D array containint the various prob of developping
-     severe side effects
-    Returns: 1D array contining the likelihood of obtaining
-    the data, x and n, for each prob P, respectively.
+    - x     int                 number of patients that develop side effects
+    - n     int                 total number of patients observed
+    - p     1D numpy.ndarray    Array with the various hypothetical
+                                probabilities of developing side effects
+    Note:
+        - If n is not a positive integer, raise a ValueError with the message
+        n must be a positive integer
+        - If x is not an integer that is greater than or equal to 0, raise a
+        ValueError with the message x must be an integer that is greater than
+        or equal to 0
+        - If x is greater than n, raise a ValueError with the message x cannot
+        be greater than n
+        - If P is not a 1D numpy.ndarray, raise a TypeError with the messag
+        P must be a 1D numpy.ndarray
+        - If any value in P is not in the range [0, 1], raise a ValueError
+        with the message All values in P must be in the range [0, 1]
+    Returns:    A 1D numpy.ndarray containing the likelihood of obtaining the
+                data, x and n, for each probability in P, respectively
     """
 
-    if not isinstance(n, int) or n <= 0:
+    if not isinstance(n, (int, float)) or n <= 0:
         raise ValueError("n must be a positive integer")
 
-    if not isinstance(x, int) or x < 0:
+    if not isinstance(x, (int, float)) or x < 0:
         message = "x must be an integer that is greater than or equal to 0"
         raise ValueError(message)
 
@@ -33,11 +46,11 @@ def likelihood(x, n, P):
     if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    res = []
+    binomial_coef = np.math.factorial(n) / np.math.factorial(x)
+    binomial_coef = binomial_coef / np.math.factorial(n - x)
+    success_rate = pow(P, x)
+    failure_rate = pow(1 - P, n - x)
 
-    for i in range(len(P)):
-        tmp = np.math.factorial(n) / np.math.factorial(x)
-        tmp = tmp / np.math.factorial(n - x) * P[i]**x
-        tmp = tmp * (1 - P[i])**(n - x)
-        res.append(tmp)
-    return res
+    likelihood = binomial_coef * success_rate * failure_rate
+
+    return likelihood
