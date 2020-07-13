d0b5df3ba1f85da1bd2742e09ecc2a292d8c6fea
NadjarFares <fares.nadjar@holbertonschool.com>
2020-07-13 13:41:18 +0100

    maj

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 Class Neuron
@@ -37,11 +36,11 @@ class Neuron:
         self.__b = 0
         self.__A = 0
 
-    def get_W(self):
+    def W(self):
         return self.__W
 
-    def get_b(self):
+    def b(self):
         return self.__b
 
-    def get_A(self):
+    def A(self):
         return self.__A
